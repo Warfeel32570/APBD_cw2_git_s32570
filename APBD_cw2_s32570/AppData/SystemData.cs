0e03c43a9fa12e831ba410ea73bfdd9e54139faf
Yauheniya Nikalyenka <s32570@pjwstk.edu.pl>
2026-03-26 18:14:07 +0100

    dev [8]: Added data storage and User services.

---
+++ APBD_cw2_s32570/AppData/SystemData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using APBD_cw2_s32570.Models;
+
+namespace APBD_cw2_s32570.AppData;
+
+public class SystemData
+{
+    public List<User> Users { get; } = new();
+    public List<Equipment> Items { get; } = new();
+    public List<Rental> Rentals { get; } = new();
+}
---
+++ APBD_cw2_s32570/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+using APBD_cw2_s32570.AppData;
+using APBD_cw2_s32570.Models;
+
+namespace APBD_cw2_s32570.Services;
+
+public class UserService
+{
+    private readonly SystemData _appData;
+
+    public UserService(SystemData appData)
+    {
+        _appData = appData;
+    }
+
+    public void AddUser(User user)
+    {
+        _appData.Users.Add(user);
+    }
+
+    public List<User> GetAllUsers()
+    {
+        return _appData.Users;
+    }
+
+    public User? GetUsersByID(int userID)
+    {
+        foreach (User user in _appData.Users)
+        {
+            if (user.ID == userID) return user;
+        }
+        return null;
+    }
+}
